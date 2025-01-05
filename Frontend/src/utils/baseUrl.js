29cff4e7ef09f671f86e10fc0b124246a58e4013
kaushik0010 <paykolikaushikb@gmail.com>
2025-01-05 19:32:08 +0530

    URL update

--- Frontend/src/utils/baseUrl.js
+++ Frontend/src/utils/baseUrl.js
@@ -1,5 +1,5 @@
 const getBaseUrl = () => {
-    return "http://localhost:5000" || "https://book-store-app-backend-liart.vercel.app"
+    return "http://localhost:5000"
 }
 
 export default getBaseUrl;
