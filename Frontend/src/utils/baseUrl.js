bf60d442cc043971340e8d61a17055aaef670197
kaushik0010 <paykolikaushikb@gmail.com>
2025-01-06 20:01:01 +0530

    url updates

--- Frontend/src/utils/baseUrl.js
+++ Frontend/src/utils/baseUrl.js
@@ -1,5 +1,5 @@
 const getBaseUrl = () => {
-    return "http://localhost:5000" || "https://book-store-app-backend-c7fs.onrender.com";
+    return "https://book-store-app-backend-c7fs.onrender.com";
 }
 
 export default getBaseUrl;
